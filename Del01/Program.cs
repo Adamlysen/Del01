f417314c0346b9f94dc1634c590324db7c831510
Adam Lysén <adam.lysen@elev.ga.ntig.se>
2024-11-13 15:29:12 +0100

    sdfgdsfsdfg

---
+++ Del01/Program.cs
@@ -0,0 +1,49 @@
+﻿if (6 >= 3)
+{
+    Console.WriteLine("Hello World!");
+}
+
+Console.WriteLine("Enter your username:");
+string username = Console.ReadLine();
+
+Console.WriteLine("Enter your password:");
+string password = Console.ReadLine();
+
+if (username == "kalleanka" && password == "12345")
+{
+    Console.WriteLine("Welcome!");
+}
+else
+{
+    Console.WriteLine("Wrong username or password");
+}
+for (int i = 0; i < 32; i++)
+{
+    Console.WriteLine("Hello, World!");
+}
+string correctPassword = "12345";
+string inputPassword;
+
+do
+{
+    Console.WriteLine("Enter your password:");
+    inputPassword = Console.ReadLine();
+
+    if (inputPassword != correctPassword)
+    {
+        Console.WriteLine("Wrong password, try again.");
+    }
+} while (inputPassword != correctPassword);
+
+Console.WriteLine("Access granted!");
+for (int i = 0; i < 5; i++)
+{
+    Console.WriteLine("Enter a number:");
+    int number = int.Parse(Console.ReadLine());
+
+    if (number > 5)
+    {
+        Console.WriteLine("Högre än 5!");
+    }
+}
+
