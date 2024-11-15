1be53ffd9bc28bbc8e9b5fc8c9dc17a0f0398da5
Adam Lysén <adam.lysen@elev.ga.ntig.se>
2024-11-15 13:22:16 +0100

    sdgsfgfdsgh

--- Del01/Program.cs
+++ Del01/Program.cs
@@ -47,3 +47,55 @@ for (int i = 0; i < 5; i++)
     }
 }
 
+bool hej = false;
+string hello;
+int a;
+
+while (hej == false)
+
+{
+    Console.WriteLine("Write Something:");
+    hello = Console.ReadLine();
+    hej = int.TryParse(hello, out a);
+
+    if (hej == false)
+    {
+        Console.WriteLine("Not Working!");
+        Console.ReadLine();
+    }
+}
+
+Random random = new Random();
+
+int rdmnumber = random.Next(1, 101);
+
+int guess = 0;
+
+while (guess != rdmnumber)
+{
+    Console.WriteLine("Gissa siffran:");
+    try
+    {
+        guess = int.Parse(Console.ReadLine());
+
+        if (guess > rdmnumber)
+        {
+            Console.WriteLine("Too high!");
+        }
+
+        else if (guess < rdmnumber)
+        {
+            Console.WriteLine("Too Low!");
+        }
+
+        else
+        {
+            Console.WriteLine("Correct!");
+            Console.ReadLine();
+        }
+    }
+    catch
+    {
+        Console.WriteLine("Error!");
+    }
+}
--- Del01/obj/Debug/net8.0/Del01.AssemblyInfo.cs
+++ Del01/obj/Debug/net8.0/Del01.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Del01")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f417314c0346b9f94dc1634c590324db7c831510")]
 [assembly: System.Reflection.AssemblyProductAttribute("Del01")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Del01")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
